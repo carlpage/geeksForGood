63f6ec724c3f1650f07f77f00245d151a7511e34
bicyclesforthemind <aaa2141@gmail.com>
2017-09-23 19:51:23 -0500

    added timestamp migration

--- migrations/20170923192519_projects_timestamp.js
+++ migrations/20170923192519_projects_timestamp.js
@@ -2,11 +2,15 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('projects', function(table) {
-        
+        table.timestamps();
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.table('projects', function(table) {
+        table.dropTimestamps();
+    })
+  ])
 };
